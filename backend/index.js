f5030b38b56b8bd1684de84f40d90ceff94c1e71
enimocar <enimocar@gmail.com>
2023-06-18 22:53:17 +0200

    Primeros pasos con el backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-import expresss from "expresss";
+import express from "express";
+
+const app = express();
+
+app.use('/', ( req, res ) =>{
+    res.send('Hola Mundo');
+})
+
+app.listen(4000, () => {
+    console.log("Servidor funcionando en el puerto 4000");
+});
