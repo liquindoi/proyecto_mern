bd5ed7e3ae89c6e517b1e33b38d348ec12517e9b
enimocar <enimocar@gmail.com>
2023-06-21 19:57:17 +0200

    Incorporando Controladores.

---
+++ backend/controllers/veterinarioController.js
@@ -0,0 +1,14 @@
+
+
+const registrar = ( req, res ) => {
+    res.send("Desde API/VETERINARIOS");
+};
+
+const perfil = ( req, res ) => {
+    res.send("Desde API/VETERINARIOS/perfil");
+};
+
+export {
+    registrar,
+    perfil
+};
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,13 +1,10 @@
 import express from 'express';
 const router = express.Router();
+import { registrar , perfil} from '../controllers/veterinarioController.js';
 
-router.get('/', ( req, res ) => {
-    res.send("Desde API/VETERINARIOS");
-});
+router.get('/', registrar);
 
-router.get('/login', ( req, res ) => {
-    res.send("Desde API/VETERINARIOS/LOGIN");
-});
+router.get('/perfil', perfil);
 
 
 export default router;
