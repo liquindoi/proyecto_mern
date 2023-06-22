7c545a982d9f6bcec2d7a4829deadbc6def8afce
enimocar <enimocar@gmail.com>
2023-06-22 21:41:30 +0200

    Almacenando Registros en la BD

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,15 +1,23 @@
+import Veterinario from '../models/Veterinario.js';
 
+const registrar = async ( req, res ) => {
+    
 
-const registrar = ( req, res ) => {
-    console.log(req.body);
+    // const { email, password, nombre } = req.body;
 
-    const { email, password, nombre } = req.body;
-    console.log(email);
+    try {
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+        res.json(veterinarioGuardado);
+    } catch (error) {
+        console.log(error);
+    }
+    /*console.log(email);
     console.log(password);
-    console.log(nombre);
+    console.log(nombre);*/
 
 
-    res.json({msg: "Registrando usuario..."});
+
 };
 
 const perfil = ( req, res ) => {
