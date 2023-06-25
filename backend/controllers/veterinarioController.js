dff23b4a58c15387b49300c9070c7a6593aeb661
enimocar <enimocar@gmail.com>
2023-06-25 10:38:19 +0200

    Prevenir Registros de Usuarios Duplicados

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -2,8 +2,16 @@ import Veterinario from '../models/Veterinario.js';
 
 const registrar = async ( req, res ) => {
     
+    const { email } = req.body;
 
-    // const { email, password, nombre } = req.body;
+    // Prevenir usuarios duplicados
+
+    const existeUsuario = await Veterinario.findOne({email: email});
+    if(existeUsuario) {
+        const error = new Error('Usuario ya registrado.');
+        return res.status(400).json({msg: error.message});
+
+    }
 
     try {
         const veterinario = new Veterinario(req.body);
